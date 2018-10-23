4b000016605365015a9136801d23a52d73fb452a
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2018-10-23 18:28:41 +0100

    Update populate db script
    Update the script to work with the latest versions of postgres:9.5
    Docker repo images.
    
    This has the nice side effect of allowing a healthcheck to be run on the
    same host as the postgres server...

--- config/sql/populate_db.sh
+++ config/sql/populate_db.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
-psql -d postgresql://localhost:5432 -c 'create database digitalmarketplace;'
-psql -d postgresql://localhost:5432/digitalmarketplace -c 'create database digitalmarketplace_test;'
-psql -d postgresql://localhost:5432/digitalmarketplace -f /docker-entrypoint-initdb.d/data/*.sql
+psql --username="$POSTGRES_USER" --dbname="$POSTGRES_DB" <<-EOSQL
+	CREATE DATABASE digitalmarketplace;
+	CREATE DATABASE digitalmarketplace_test;
+EOSQL
+psql --username="$POSTGRES_USER" --dbname="digitalmarketplace" -f /docker-entrypoint-initdb.d/data/*.sql
