2758dd199c2c54a713124865759313b229ae591e
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2018-10-23 18:25:49 +0100

    Remove DMRUNNER_USER in favour of POSTGRES_USER
    DMRUNNER_USER was only used for PostgreSQL; this commit instead uses the
    POSTGRES_USER environment variable that is supported by the postgresql
    image on Docker Hub.
    
    See the [repo documentation](https://hub.docker.com/_/postgres/) for
    more details.

--- config/sql/populate_db.sh
+++ config/sql/populate_db.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 
-CREATE_USER_CMD="create user "
-CREATE_USER_CMD+=$DMRUNNER_USER
-CREATE_USER_CMD+=" superuser;"
-
-psql -d postgresql://postgres@localhost:5432 -c "$CREATE_USER_CMD"
 psql -d postgresql://localhost:5432 -c 'create database digitalmarketplace;'
 psql -d postgresql://localhost:5432/digitalmarketplace -c 'create database digitalmarketplace_test;'
 psql -d postgresql://localhost:5432/digitalmarketplace -f /docker-entrypoint-initdb.d/data/*.sql
