fc51c56f09663753d513a1e85bcb41f557d993f6
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2019-11-07 10:49:28 +0000

    Revert "Add an error message if postgres is present but not configured"

--- dmrunner/process.py
+++ dmrunner/process.py
@@ -18,7 +18,7 @@ import threading
 import time
 from typing import List, Optional, Sequence, Union
 
-from .utils import PROCESS_TERMINATED, PROCESS_NOEXIST, EXITCODE_NOT_ANTICIPATED_EXECUTION, bold, red
+from .utils import PROCESS_TERMINATED, PROCESS_NOEXIST, EXITCODE_NOT_ANTICIPATED_EXECUTION, bold
 
 
 Path = Union[os.PathLike, str]
@@ -90,7 +90,7 @@ class DMServices(DMExecutable):
         return subprocess.call(cls._get_docker_compose_command(docker_compose_filepaths, "build"))
 
     @staticmethod
-    def services_healthcheck(shutdown_event, logger, check_once=False):
+    def services_healthcheck(shutdown_event, check_once=False):
         """Attempts to validate that required background services (NGINX, Elasticsearch, Postgres) are all
         operational. It takes some shortcuts in doing so, but should be effective in most cases."""
         healthcheck_result = {"nginx": False, "elasticsearch": False, "postgres": False}
@@ -124,20 +124,8 @@ class DMServices(DMExecutable):
                     ).close()
                     healthcheck_result["postgres"] = True
 
-                except psycopg2.OperationalError as e:
-                    pgerror = str(e).strip()
-                    if pgerror.startswith("could not connect to server"):
-                        healthcheck_result["postgres"] = False
-                    else:
-                        logger("Making a connection to Postgres failed with an unexpected error")
-                        logger(red(f"\t{pgerror}"), "postgres")
-                        logger("Please make sure that your local Postgres instance is set-up correctly")
-                        logger("and that you are not running a local Postgres server without realising it!")
-
-                        # abort
-                        if check_once:
-                            healthcheck_result["postgres"] = True
-                        shutdown_event.set()
+                except psycopg2.OperationalError:
+                    healthcheck_result["postgres"] = False
 
                 if all(healthcheck_result.values()):
                     break
@@ -200,7 +188,7 @@ class DMServices(DMExecutable):
 
     def blocking_healthcheck(self, shutdown_event):
         self._thread_healthcheck = threading.Thread(
-            target=self.services_healthcheck, args=(shutdown_event, self._logger), name="Thread-Services-HC"
+            target=self.services_healthcheck, args=(shutdown_event,), name="Thread-Services-HC"
         )
         self._thread_healthcheck.start()
 
